ae11646f067dbf1ae62eccfbfe8d412f641564b7
nmmarquez <nmmarquez@gmail.com>
2016-07-10 17:09:12 -0700

    add bootstrap elements to website get basic layout setup

--- neal/website_root/resources/js/map.js
+++ neal/website_root/resources/js/map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([47.6,-122.3], 13);
+var mymap = L.map('map').setView([47.6,-122.3], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
   maxZoom: 18,
