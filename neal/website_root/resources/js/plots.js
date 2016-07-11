985e9d1ad632f3ddea75ad06c6e95161e63eddac
nmmarquez <nmmarquez@gmail.com>
2016-07-11 11:17:38 -0700

    mess around with d3 and highcharts

---
+++ neal/website_root/resources/js/plots.js
@@ -0,0 +1,36 @@
+
+$(function () {
+  $('#graph1').highcharts({
+      title: {
+          text: 'Crime Occurences',
+          x: -20 //center
+      },
+      xAxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      },
+      yAxis: {
+          title: {
+              text: 'Occurences'
+          },
+          plotLines: [{
+              value: 0,
+              width: 1,
+              color: '#808080'
+          }]
+      },
+      tooltip: {
+          valueSuffix: ''
+      },
+      legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'middle',
+          borderWidth: 0
+      },
+      series: [{
+          name: 'Capitol Hill',
+          data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+      }]
+  });
+});
