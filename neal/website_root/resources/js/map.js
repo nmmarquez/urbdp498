2e42c1ca176093ee88f057e5ff16bd6cb5fe08da
nmmarquez <nmmarquez@gmail.com>
2016-07-22 09:35:55 -0700

    refactor teh code so that the js isnt all in one script

--- neal/website_root/resources/js/map.js
+++ neal/website_root/resources/js/map.js
@@ -13,8 +13,9 @@ function text_for_html(str){
 	return new_str
 }
 
+var map_token = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw'
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + map_token, {
   maxZoom: 18, minZoom: 11,
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -27,13 +28,6 @@ var zoomControl = L.control.zoom({
     position: 'topright'
 });
 mymap.addControl(zoomControl);
-// Add our loading control in the same position and pass the
-// zoom control to attach to it
-// var loadingControl = L.Control.loading({
-//     position: 'topright',
-//     zoomControl: zoomControl
-// });
-// mymap.addControl(loadingControl);
 
 
 //put all your markers on a specific layers in case you want to remove all the markers quickly
@@ -41,107 +35,24 @@ mymap.addControl(zoomControl);
 var markers = new L.FeatureGroup();
 
 
-(function () {
-    $(function () {
-        var SideBAR;
-        SideBAR = (function () {
-            function SideBAR() {}
-
-            SideBAR.prototype.expandMyMenu = function () {
-                return $("nav.sidebar").removeClass("sidebar-menu-collapsed").addClass("sidebar-menu-expanded");
-            };
-
-            SideBAR.prototype.collapseMyMenu = function () {
-                return $("nav.sidebar").removeClass("sidebar-menu-expanded").addClass("sidebar-menu-collapsed");
-            };
-
-            SideBAR.prototype.showMenuTexts = function () {
-                return $("nav.sidebar ul a span.expanded-element").show();
-            };
-
-            SideBAR.prototype.hideMenuTexts = function () {
-                return $("nav.sidebar ul a span.expanded-element").hide();
-            };
-
-            SideBAR.prototype.showActiveSubMenu = function () {
-                $("li.active ul.level2").show();
-                return $("li.active a.expandable").css({
-                    width: "100%"
-                });
-            };
-
-            SideBAR.prototype.hideActiveSubMenu = function () {
-                return $("li.active ul.level2").hide();
-            };
-
-            SideBAR.prototype.adjustPaddingOnExpand = function () {
-                $("ul.level1 li a.expandable").css({
-                    padding: "1px 4px 4px 0px"
-                });
-                return $("ul.level1 li.active a.expandable").css({
-                    padding: "1px 4px 4px 4px"
-                });
-            };
-
-            SideBAR.prototype.resetOriginalPaddingOnCollapse = function () {
-                $("ul.nbs-level1 li a.expandable").css({
-                    padding: "4px 4px 4px 0px"
-                });
-                return $("ul.level1 li.active a.expandable").css({
-                    padding: "4px"
-                });
-            };
-
-            SideBAR.prototype.ignite = function () {
-                return (function (instance) {
-                    return $("#justify-icon").click(function (e) {
-                        if ($(this).parent("nav.sidebar").hasClass("sidebar-menu-collapsed")) {
-                            instance.adjustPaddingOnExpand();
-                            instance.expandMyMenu();
-                            instance.showMenuTexts();
-                            instance.showActiveSubMenu();
-                            $(this).css({
-                                color: "white"
-                            });
-                        } else if ($(this).parent("nav.sidebar").hasClass("sidebar-menu-expanded")) {
-                            instance.resetOriginalPaddingOnCollapse();
-                            instance.collapseMyMenu();
-                            instance.hideMenuTexts();
-                            instance.hideActiveSubMenu();
-                            $(this).css({
-                                color: "#FFF"
-                            });
-                        }
-                        return false;
-                    });
-                })(this);
-            };
-
-            return SideBAR;
-
-        })();
-        return (new SideBAR).ignite();
-    });
-
-}).call(this);
-
 $(document).ready(function(){
 
-	marker_crime()
-	draw_timeline()
-  draw_hist()
+	marker_crime();
+	draw_timeline();
+  draw_hist();
 
-        $.getJSON(root_api + "locations/list", function( data ) {
-          $.each( data, function( key, val ) {
-            $('#locations select').append('<option value="' + key + '">'+val.neighborhood+'</option>');
-          });
-        });
+  $.getJSON(root_api + "locations/list", function( data ) {
+    $.each( data, function( key, val ) {
+      $('#locations select').append('<option value="' + key + '">'+val.neighborhood+'</option>');
+    });
+  });
 
-        $.getJSON(root_api + "crime/types", function( data ) {
-          $.each( data, function( key, val ) {
-            $('#crimes select').append('<option value="' + key + '">'+val.clearance_group_description+'</option>');
-          });
-        });
+  $.getJSON(root_api + "crime/types", function( data ) {
+    $.each( data, function( key, val ) {
+      $('#crimes select').append('<option value="' + key + '">'+val.clearance_group_description+'</option>');
+    });
+  });
+
 	$('#location_select option').on('click', function(){
 	    $('#neighborhood').val($(this).text());
 	});
@@ -150,23 +61,34 @@ $(document).ready(function(){
     	    var selectedText = $(this).find("option:selected").text();
           $("#selected_neighborhood").text(selectedText);
           set_neigh_view()
-
-	    // $("#test").text(selectedText);
 	});
 
 	$('#crime_select').change(function () {
-    	    var selectedText = $(this).find("option:selected").text();
-
+    	var selectedText = $(this).find("option:selected").text();
 	    $("#selected_crime").text(selectedText);
       marker_crime();
 	    draw_timeline();
+      draw_hist();
 	});
+
+  function set_neigh_view(){
+    var lat = 0;
+    var long = 0;
+    var html_neigh_var = text_for_html($("#selected_neighborhood").text());
+
+    $.getJSON( root_api + "locations/coordinates/" + html_neigh_var, function( data ) {
+        lat = data[0].latitude
+        long = data[0].longitude
+        mymap.setView([lat,long], 15);
+
+    });
+
+  }
 
 	function marker_crime() {
 		markers.clearLayers();
 
 		html_crime_var = text_for_html($("#selected_crime").text());
-		console.log(html_crime_var);
 
 		// call the api where the data is stored
 		$.getJSON( root_api + "crime/all/" + html_crime_var, function( data ) {
@@ -196,176 +118,16 @@ $(document).ready(function(){
 	}; //end of function marker_crime();
 
 
-
-
-	function draw_timeline() {
-
-		var crime_date = [];
-		var count = [];
-		var html_crime_var = text_for_html($("#selected_crime").text());
-
-		$.getJSON( root_api + "crime/timeline/" + html_crime_var, function( data ) {
-
-			//look at the data returned by your API
-			//console.log(data);
-
-			//What is the for loop doing?
-			// this function is looping through the results to build an array filled with the results from the api
-			for (i = 0; i < data.length; i++){
-                        crime_date.push(data[i].year_id);
-                        count.push(data[i].NumberOfCrimes);
-            		}
-
-
-			$(function () {
-			  $('#info-pane').highcharts({
-			      chart:{backgroundColor:'rgba(0, 0, 0, 0.7)'},
-			      title: {
-				  text: $("#selected_crime").text() + " Timeline",
-				  x: -20 //center
-			      },
-			      xAxis: {
-				  categories: crime_date
-			      },
-			      yAxis: {
-				  title: {
-				      text: 'Occurences'
-				  },
-				  plotLines: [{
-				      value: 0,
-				      width: 1,
-				      color: '#808080'
-				  }]
-			      },
-			      tooltip: {
-				  valueSuffix: ''
-			      },
-			      legend: {
-				  layout: 'vertical',
-				  align: 'right',
-				  verticalAlign: 'middle',
-				  borderWidth: 0
-			      },
-			      series: [{
-				  name: 'All Seattle',
-				  data: count
-			      }]
-			  });
-			});
-		});
-	};
-
-  function set_neigh_view(){
-    var lat = 0;
-    var long = 0;
-    var html_neigh_var = text_for_html($("#selected_neighborhood").text());
-
-    $.getJSON( root_api + "locations/coordinates/" + html_neigh_var, function( data ) {
-        lat = data[0].latitude
-        long = data[0].longitude
-        mymap.setView([lat,long], 15);
-
-    });
-
-  }
-
-  function draw_hist() {
-
-		var neighborhoods = [];
-		var crime_numbers = [];
-    var html_crime_var = text_for_html($("#selected_crime").text());
-
-		$.getJSON( root_api + "crime/neighborhoodcount/" + html_crime_var, function( data ) {
-
-			//look at the data returned by your API
-			//console.log(data);
-
-			//What is the for loop doing?
-			// this function is looping through the results to build an array filled with the results from the api
-			for (i = 0; i < data.length; i++){
-                        neighborhoods.push(data[i].neighborhood);
-                        crime_numbers.push(data[i].crime_number);
-            }
-
-
-            //Replace XX by an appropriate div to display the histogram in
-			$('#display-pane').highcharts({
-		        chart: {
-		            type: 'column', backgroundColor:'rgba(0, 0, 0, 0.7)'
-		        },
-		        title: {
-		            text: $("#selected_crime").text() + ' per Neigborhhod'
-		        },
-		        xAxis: {
-		        	//replace XX by the array of categories
-		            categories: neighborhoods,
-		            title: {
-		                text: null
-		            },
-			    labels: {
-				style: {
-				    color: 'black',
-				    fontSize:'15px'
-				}
-            		    }
-		        },
-		        yAxis: {
-		            min: 0,
-		            title: {
-		                text: 'Number of trees',
-		                align: 'high',
-				style: {"color": "black", "fontWeight": "bold", "font-size": "15px" }
-		            },
-		            labels: {
-		                overflow: 'justify',
-                                style: {
-				    color: 'black',
-				    fontSize:'10px'
-				}
-		            }
-		        },
-		        tooltip: {
-		            valueSuffix: ' trees',
-		            formatter: function() {
-		            	//try other formatters if you want
-		    					return 'There are <b>'+ this.y +
-		           		'</b> Occurences in <b>' + this.x + '</b>';
-		  					}
-		        },
-		        plotOptions: {
-		            bar: {
-		                dataLabels: {
-		                    enabled: true
-		                }
-		            }
-		        },
-		        legend: {
-		            layout: 'vertical',
-		            align: 'right',
-		            verticalAlign: 'top',
-		            x: -40,
-		            y: 80,
-		            floating: true,
-		            borderWidth: 1,
-		            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-		            shadow: true
-		        },
-		        credits: {
-		            enabled: false
-		        },
-		        series: [{
-		        	//replace XX by the array of values
-		        	data: crime_numbers,
-		          showInLegend:false
-		        }]
-		    }); // end of histogram function
-
-		}) // end of getJSON function
-
-	}; // end of draw_hist function
-
-  $("#info-pane").mouseenter(function() {draw_timeline()});
-  $("#info-pane").mouseleave(function() {draw_timeline()});
-  $("#display-pane").mouseenter(function() {draw_hist()});
-  $("#display-pane").mouseleave(function() {draw_hist()});
+  $("#info-pane").mouseenter(function() {
+    draw_timeline()
+  });
+  $("#info-pane").mouseleave(function() {
+    draw_timeline()
+  });
+  $("#display-pane").mouseenter(function() {
+    draw_hist()
+  });
+  $("#display-pane").mouseleave(function() {
+    draw_hist()
+  });
 });
--- neal/website_root/resources/js/plots.js
+++ neal/website_root/resources/js/plots.js
@@ -1,36 +1,83 @@
+function draw_timeline() {
 
-$(function () {
-  $('#graph1').highcharts({
-      title: {
-          text: 'Crime Occurences',
-          x: -20 //center
-      },
-      xAxis: {
-          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-      },
-      yAxis: {
-          title: {
-              text: 'Occurences'
+  var crime_date = [];
+  var count = [];
+  var html_crime_var = text_for_html($("#selected_crime").text());
+
+  $.getJSON( root_api + "crime/timeline/" + html_crime_var, function( data ) {
+
+    for (i = 0; i < data.length; i++){
+                      crime_date.push(data[i].year_id);
+                      count.push(data[i].NumberOfCrimes);
+              }
+
+    $(function () {
+      $('#info-pane').highcharts({
+          chart:{backgroundColor:'rgba(0, 0, 0, 0.7)'},
+          title: { text: $("#selected_crime").text() + " Timeline", x: -20 },
+          xAxis: { categories: crime_date },
+          yAxis: {
+            title:   { text: 'Occurences' },
+            plotLines: [{ value: 0,width: 1, color: '#808080'}]
           },
-          plotLines: [{
-              value: 0,
-              width: 1,
-              color: '#808080'
-          }]
-      },
-      tooltip: {
-          valueSuffix: ''
-      },
-      legend: {
-          layout: 'vertical',
-          align: 'right',
-          verticalAlign: 'middle',
-          borderWidth: 0
-      },
-      series: [{
-          name: 'Capitol Hill',
-          data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-      }]
+          tooltip: { valueSuffix: '', style: { fontSize: '15pt' },},
+          legend: { layout: 'vertical', align: 'right', verticalAlign: 'middle', borderWidth: 0 },
+          series: [{ name: 'All Seattle', data: count }]
+      });
+    });
   });
-});
+};
+
+function draw_hist() {
+
+  var neighborhoods = [];
+  var crime_numbers = [];
+  var html_crime_var = text_for_html($("#selected_crime").text());
+
+  $.getJSON( root_api + "crime/neighborhoodcount/" + html_crime_var, function( data ) {
+
+    for (i = 0; i < data.length; i++){
+                      neighborhoods.push(data[i].neighborhood);
+                      crime_numbers.push(data[i].crime_number);
+          }
+
+    $('#display-pane').highcharts({
+          chart: { type: 'column', backgroundColor:'rgba(0, 0, 0, 0.7)'},
+          title: { text: $("#selected_crime").text() + ' per Neigborhhod' },
+          xAxis: {
+            categories: neighborhoods,
+            title: { text: null },
+            labels: {
+              style: { fontSize:'15px' }
+            }
+          },
+          yAxis: {
+            min: 0,
+            title: {
+              text: 'Number of Crimes',
+              },
+              labels: {
+                  overflow: 'justify',
+                  style: { fontSize:'15px' }
+                }
+              },
+          tooltip: {
+              style: { fontSize: '15pt' },
+              valueSuffix: ' trees',
+              formatter: function() {
+                return 'There are <b>'+ this.y +
+                '</b> Occurences in <b>' + this.x + '</b>';
+              }
+          },
+          plotOptions: {
+              bar: {
+                  dataLabels: { enabled: true }
+              }
+          },
+          credits: { enabled: false },
+          series: [{ data: crime_numbers, showInLegend:false }]
+      }); // end of timeline function
+
+  }) // end of getJSON function
+
+}; // end of draw_timeline function
